849ad761cb82d1f0e55ac1ee54103ec7759ae182
juanpb27 <juancpb0727@gmail.com>
2024-10-11 18:06:00 -0500

    Cambio de vllm a transformers

--- app.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def run_vllm():
-    # Comando para ejecutar el modelo usando vLLM con CPU
-    subprocess.run(["vllm", "serve", "fireworks-ai/firefunction-v1"])
-
-if __name__ == "__main__":
-    print("Iniciando el servidor vLLM...")
-    run_vllm()
